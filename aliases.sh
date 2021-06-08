fd7e12e0d8eb59ea032e362a9dfbf35405269df7
David Attlee <dattlee@bloomberg.net>
2021-06-08 12:12:12 -0400

    Add aliases

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,17 @@
+# Docker aliases
 alias dc=docker-compose
 alias dk=docker
-alias prettier="npm run prettier"
-alias refresh="source $HOME/.zshrc"
+alias dm='./docker.mk '
 
+# Basic config aliases
+# alias prettier="npm run prettier"
+alias refresh="source $HOME/.zshrc"
+alias rm="rm -i"
+alias cat=bat
+
+# Python aliases
+alias py=python3.9
+
+# Git aliases
+alias gfu='git fetch upstream'
+alias gmu='git merge upstream/$(git_main_branch)'
