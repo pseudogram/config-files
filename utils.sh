83504a88d0db4a89551c703bc0f468fc6cda1174
David Attlee <dattlee@bloomberg>
2023-04-05 17:41:26 -0400

    add git wortree cloning util

--- utils.sh
+++ utils.sh
@@ -15,3 +15,28 @@ mcd ()
   mkdir -p -- "$1" &&
     cd -P -- "$1"
 }
+
+
+
+# Git worktree clone. Sets up everything to get tarted in a repo
+wclone(){
+  # Get repo name
+  url=$1
+  basename=$(basename $url)
+  echo $basename
+  filename=${basename%.*}
+  echo Cloning to file: $filename
+  # extension=${basename##*.}
+  # echo $extension
+
+  # Make direcotry
+  mkdir $filename
+  cd $filename
+  git clone $url .bare --bare
+  ln -s .bare .git
+
+  # Add some default branches to work with
+  git worktree add main
+  git worktree add current
+  git worktree add hotfix
+}
