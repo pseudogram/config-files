e5314b2daff51e8c2fb615960179787741076561
David Attlee <dattlee@bloomberg>
2023-06-20 11:42:41 -0400

    add nx plugin to ohmyzsh

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -3,7 +3,7 @@ export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="powerlevel10k/powerlevel10k"
 # ZSH_THEME="gozilla"
 
-plugins=(git nvm)
+plugins=(git nvm nx-completion)
 zstyle ':omz:plugins:nvm' lazy true
 
 source $ZSH/oh-my-zsh.sh
