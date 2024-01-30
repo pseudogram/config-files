d4f21d6589c2fab224574869de8c10c174b62eb2
Pseudogram <pseudogrammer@gmail.com>
2024-01-30 12:25:52 -0500

    update ohmyzsh

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -1,9 +1,7 @@
 # Oh my zsh
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="powerlevel10k/powerlevel10k"
-# ZSH_THEME="gozilla"
 
-plugins=(git nvm nx-completion)
-zstyle ':omz:plugins:nvm' lazy true
+plugins=(git rails bundler asdf)
 
 source $ZSH/oh-my-zsh.sh
